b7de9b5da3fbaa042060b0a9fd048d151babb830
Mikhail Macnev <misha@macnev.ru>
2021-04-23 01:36:45 +0500

    added inverse entity to `ManagedFeedImage` and `ManagedFeedCache`

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -97,6 +97,7 @@ class ManagedFeedImage: NSManagedObject {
 	@NSManaged var imageDescription: String?
 	@NSManaged var location: String?
 	@NSManaged var url: URL
+	@NSManaged var cache: ManagedFeedCache
 
 	var local: LocalFeedImage {
 		LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
